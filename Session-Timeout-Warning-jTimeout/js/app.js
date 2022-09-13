efa8e6686a82687410635c3cbca2929bc4671838
Ahmar Jabbar <87254977+ahmar7@users.noreply.github.com>
2022-09-13 21:36:46 +0500

    js update

--- Session-Timeout-Warning-jTimeout/js/app.js
+++ Session-Timeout-Warning-jTimeout/js/app.js
@@ -1,8 +1,37 @@
 
 
+let popup = document.getElementById("jTimedoutAlert");
 
+let showPopup =()=>{
+    popup.style.display="block"
+}
 const myTimeout = setTimeout(showPopup, 5000);
 
 function myStopFunction() {
   clearTimeout(myTimeout);
 }
+
+
+
+// =====================
+function startTimer(duration, display) {
+  var timer = duration,
+    minutes,
+    seconds;
+  setInterval(function () {
+    minutes = parseInt(timer / 60, 10);
+    seconds = parseInt(timer % 60, 10);
+
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+    display.textContent = minutes + ":" + seconds;
+    if (--timer < 0) {
+      timer = duration;
+    }
+  }, 1000);
+}
+window.onload = function () {
+  var fiveMinutes = 60 * 5,
+    display = document.querySelector("#time");
+  startTimer(fiveMinutes, display);
+};
