630f39a9da71bdf8f91e5a3a36c07f58ed780ce4
Ahmar Jabbar <87254977+ahmar7@users.noreply.github.com>
2022-09-14 13:37:00 +0500

    update

--- Session-Timeout-Warning-jTimeout/js/app.js
+++ Session-Timeout-Warning-jTimeout/js/app.js
@@ -1,37 +1,92 @@
-
-
 let popup = document.getElementById("jTimedoutAlert");
+let times = document.getElementById("time");
 
-let showPopup =()=>{
-    popup.style.display="block"
+let hidepopup=()=>{
+
+    popup.style.display = "none";
 }
-const myTimeout = setTimeout(showPopup, 5000);
+let showPopup = () => {
+    popup.style.display = "block";
+};
+const myTimeout = setTimeout(showPopup, 1000);
 
-function myStopFunction() {
-  clearTimeout(myTimeout);
+function myStopFunction() { 
+    setTimeout(() => {
+        showPopup()
+    }, 1000);
 }
-
-
 
 // =====================
+
 function startTimer(duration, display) {
   var timer = duration,
     minutes,
     seconds;
-  setInterval(function () {
+  const myInterval = setInterval(myTimer, 1000);
+  function myTimer() {
     minutes = parseInt(timer / 60, 10);
     seconds = parseInt(timer % 60, 10);
 
     minutes = minutes < 10 ? "0" + minutes : minutes;
     seconds = seconds < 10 ? "0" + seconds : seconds;
     display.textContent = minutes + ":" + seconds;
+console.log("real",seconds, minutes, times.innerHTML);
+   if (times.innerHTML == "00:00") {
+      console.log("cefc");
+    }
+
+    if (seconds == 0 && minutes == 0) {
+        clearInterval(myInterval);
+         
+    }
     if (--timer < 0) {
       timer = duration;
     }
-  }, 1000);
+  }
 }
-window.onload = function () {
-  var fiveMinutes = 60 * 5,
+
+
+function startCount() { 
+    var fiveMin = 15;
+     
     display = document.querySelector("#time");
-  startTimer(fiveMinutes, display);
-};
+    startTimer(fiveMin, display); 
+    console.log(fiveMin);
+    
+}
+const countDown = setTimeout(startCount, 1000);
+ 
+function mystart() {
+function startTimer(duration, display) {
+  var timer = duration,
+    minutes,
+    seconds;
+  const myInterval = clearInterval(myTimer, 1000);
+  function myTimer() {
+    minutes = parseInt(timer / 60, 10);
+    seconds = parseInt(timer % 60, 10);
+
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+    display.textContent = minutes + ":" + seconds;
+    console.log("real", seconds, minutes, times.innerHTML);
+    if (times.innerHTML == "00:00") {
+      console.log("cefc");
+    }
+
+    if (seconds == 0 && minutes == 0) {
+      clearInterval(myInterval);
+    }
+    if (--timer < 0) {
+      timer = duration;
+    }
+  }
+}
+clearInterval(startTimer,1000)
+}
+ let inputval = document.getElementById("inputval");
+ let function1 = () => {
+//    hidepopup();
+   myStopFunction(); 
+    mystart()
+ };
